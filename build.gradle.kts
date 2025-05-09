0fb16313ee6a82d89425e5627be598539191c6bc
pro3399 <mertkanak@anadoluhayat.com.tr>
2025-05-09 11:08:39 +0300

    Refactor: Gradle yapılandırmasını güncelle - Repository tanımlamalarını settings.gradle'e taşıdım - Root/build.gradle'daki allprojects bloğunu kaldırdım - app/build.gradle’da versionCode/versionName okumasını version.properties’ten direkt yapacak şekilde düzelttim - org.jetbrains.kotlin.plugin.compose satırını kaldırıp composeOptions ekledim

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// DreamMate/build.gradle.kts
-
-plugins {
-    // Uygulama modülleri için plugin tanımı burada yapılmaz!
-    // Sadece "buildscript" veya "plugins" bloğu ile versiyon yönetimi yapılabilir.
-    // Alternatif olarak boş bırakılabilir.
-}
-
